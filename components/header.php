737d4ad134d03ce629567913d0c243e27d593e58
ss-ameer <sibuma.syedameer@gmail.com>
2024-04-13 16:50:52 +0800

    config: header for readability/aestethics

--- components/header.php
+++ components/header.php
@@ -1,62 +1,68 @@
 <!-- header.php -->
 <?php
 
-    linkStyle('bootstrap', '../');
-    linkStyle('main', '../');
-
     $sql_connection = mysqli_connect('localhost', 'root', '', 'wheelwise_db');
     $tableName = 'users';
 
-    // navigation
     define('NAV_ITEMS', [
         'default' => [
-            'home' => addPage('home'),
-            'contact_us' => addPage('contact_us')
-        ],
-        'logged_in' => [
-            'profile' => addPage('profile'),
-            'logout' => addConfig('handle_logout') 
-        ],
-        'logged_out' => [
-            'signup' => addPage('signup'),
-            'login' => addPage('login'),
-        ]
-    ]);
+                'home' => addPage('home'),
+                'contact_us' => addPage('contact_us')
+            ],
+            'logged_in' => [
+                'profile' => addPage('profile'),
+                'logout' => addConfig('handle_logout') 
+            ],
+            'logged_out' => [
+                'signup' => addPage('signup'),
+                'login' => addPage('login'),
+            ]
+        ]);
+    
+    function nav_displayItem($name, $link) {
+        $replace = '_';
+        $replacement = ' ';
+        return "<li><a href='{$link}'>" . ucwords(str_replace($replace, $replacement, $name)) . "</a></li>";
+    }
+
+    linkStyle('bootstrap', '../');
+    linkStyle('main', '../');
+
 ?>
 
-
 <h1><?= MAIN_TITLE; ?></h1>
+
 <nav>
     <ul>
+
         <?php foreach (NAV_ITEMS as $nav_type => $nav_item): ?>
+
             <?php if ($nav_type === 'default'): ?>
                 <?php foreach ($nav_item as $nav_name => $nav_link): ?>
-                    <li>
-                        <a href="<?= $nav_link; ?>" id="<?= $nav_name ?>"><?= ucwords(str_replace('_', ' ', $nav_name)); ?></a>
-                    </li>
+                    <?= nav_displayItem($nav_name, $nav_link); ?>
                 <?php endforeach; ?>
+
             <?php elseif ($nav_type === 'logged_in' && isset($_SESSION['user_id'])): ?>
                 <?php foreach ($nav_item as $nav_name => $nav_link): ?>
-                    <li>
-                        <a href="<?= $nav_link; ?>" id="<?= $nav_name ?>"><?= ucwords($nav_name); ?></a>
-                    </li>
+                    <?= nav_displayItem($nav_name, $nav_link); ?>
                 <?php endforeach; ?>
+
             <?php elseif ($nav_type === 'logged_out' && !isset($_SESSION['user_id'])): ?>
                 <?php foreach ($nav_item as $nav_name => $nav_link): ?>
-                    <li>
-                        <a href="<?= $nav_link; ?>" id="<?= $nav_name ?>"><?= ucwords($nav_name); ?></a>
-                    </li>
+                    <?= nav_displayItem($nav_name, $nav_link); ?>
                 <?php endforeach; ?>
             <?php endif ?>
+
         <?php endforeach; ?>
+
     </ul>
 </nav>
 
-<?php 
-  if (sql_sessionValidityCheck($sql_connection, $tableName)) 
-  {
-    header('Location: ' . addPage('handle_logout'));
-  } 
+<?php
+
+  if (sql_sessionValidityCheck($sql_connection, $tableName)) {
+    header('Location: ' . addPage('handle_logout')); } 
+
 ?>
 
 <!-- /header.php -->
